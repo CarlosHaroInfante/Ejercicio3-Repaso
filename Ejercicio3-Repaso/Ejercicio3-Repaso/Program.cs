1d6bfafda04509b2b5993ca42261e16b26dcfb96
CarlosHaroInfante <145343573+CarlosHaroInfante@users.noreply.github.com>
2024-01-17 19:10:37 +0100

    Add files via upload

---
+++ Ejercicio3-Repaso/Ejercicio3-Repaso/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ejercicio3Repaso{
+class Program
+    {
+  
+       static void Main(string[] args)
+       {
+           
+
+           Console.WriteLine("¿Cuanto KG pesan los sacos?");
+           int peso = Convert.ToInt32(Console.ReadLine());
+
+
+           long furgoneta = 4000;
+
+           long numSaco = (furgoneta / peso);
+
+           Console.WriteLine("La furgoneta puede llevar: " + numSaco + " Sacos");
+
+
+
+       }
+
+
+    }
+}
